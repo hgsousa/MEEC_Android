7174a8a9991fb933db20aaa3c0f283edbef34e1c
Diogo Vale <a14810@alunos.ipca.pt>
2020-12-01 22:19:36 +0000

    Versão 1.1.2 (Register layout update)

--- app/src/main/java/com/example/cm_meec_2021/ThirdActivity_register.kt
+++ app/src/main/java/com/example/cm_meec_2021/ThirdActivity_register.kt
@@ -2,10 +2,38 @@ package com.example.cm_meec_2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class ThirdActivity_register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third_register)
+    }
+
+    fun onClickRegisterButton(view: View){
+        val nameEditText = findViewById<EditText>(R.id.RegisterNametag)
+        val emailEditText = findViewById<EditText>(R.id.RegisterEmailAddress);
+        val passwordEditText = findViewById<EditText>(R.id.RegisterPassword);
+        val confirmedEditText = findViewById<EditText>(R.id.RegisterConfirmPassword);
+
+        val nameText = nameEditText.text.toString()
+        val emailText = emailEditText.text.toString()
+        val passwordText = passwordEditText.text.toString()
+        val comfirmedText = confirmedEditText.text.toString()
+
+        if (!StringUtilis.validateEmail(emailText)) {
+            Toast.makeText(this, "Email is not valid!", Toast.LENGTH_LONG).show()
+            return
+        }
+        else if(!StringUtilis.validatePassword(passwordText)) {
+            Toast.makeText(this, "Password is not valid!", Toast.LENGTH_LONG).show()
+            return
+        }
+        else if(passwordText != comfirmedText) {
+            Toast.makeText(this, "Password is not valid!", Toast.LENGTH_LONG).show()
+            return
+        }
     }
 }
